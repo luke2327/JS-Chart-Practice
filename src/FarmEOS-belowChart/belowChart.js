708da07a42a59f0f9d7ebeceae57a571cb83a4cd
luke2327 <qwepoi8526@gmail.com>
2019-02-26 16:44:12 +0900

    y Axis支店をもう一つ作った。(README参照)

--- src/FarmEOS-belowChart/belowChart.js
+++ src/FarmEOS-belowChart/belowChart.js
@@ -1,17 +1,24 @@
-Chart.pluginService.register({
-  beforeDraw: function (chart) {
-    if (chart.config.options.chartArea && chart.config.options.chartArea.backgroundColor) {
-      var ctx = chart.chart.ctx;
-      var chartArea = chart.chartArea;
+// Chart.pluginService.register({
+//   beforeDraw: function (chart) {
+//     if (chart.config.options.chartArea && chart.config.options.chartArea.backgroundColor) {
+//       var ctx = chart.chart.ctx;
+//       var chartArea = chart.chartArea;
 
-      ctx.save();
-      ctx.fillStyle = chart.config.options.chartArea.backgroundColor;
-      ctx.fillRect(chartArea.left, chartArea.top, chartArea.right - chartArea.left, chartArea.bottom - chartArea.top);
-      ctx.restore();
-    }
-  }
-});
+//       ctx.save();
+//       ctx.fillStyle = chart.config.options.chartArea.backgroundColor;
+//       ctx.fillRect(chartArea.left, chartArea.top, chartArea.right - chartArea.left, chartArea.bottom - chartArea.top);
+//       ctx.restore();
+//     }
+//   }
+// });
+// 위는 플러그인인데 필요없어서 쓰지 않음
+// 원래 차트 배경색 지정하려고 사용했던 것 인데 <div>태그 안에 <canvas>태그를 만들고 <div>에 배경색을 입히니 간단하게 해결 가능
 
+/**
+ * 
+ * @param {string} chart 
+ * @description fetch로 API Call을 해서 FARM EOS의 정보를 받아옴
+ */
 async function updateData(chart){
   var reqHeader = new Headers();
   reqHeader = {
@@ -25,20 +32,42 @@ async function updateData(chart){
 
   var data = (await fetch('https://www.farmeos.io/api/v1/crash/query_report', reqHeader)).json();
   data.then(res => {
-    chart.data.datasets[0].data = res.data.series[0].data;
-    chart.data.datasets[1].data = res.data.series[1].data;
-    chart.data.labels = res.data.xAxis[0].data;
-  });
+    // 데이터가 바뀌었을 때만 업데이트 하기위해 조건문 사용
+    if (chart.data.datasets[0].data[0] != res.data.series[0].data[0] && 
+        chart.data.datasets[1].data[0] != res.data.series[1].data[0]){
+      chart.data.datasets[0].data = res.data.series[0].data;
+      chart.data.datasets[1].data = res.data.series[1].data;
+      chart.data.labels = res.data.xAxis[0].data;
+      
+      // multiple의 데이터를 한번씩 순찰하면서 10이 넘으면 10.00으로 만듦
+      for(var i = 0; i <= 25; i++ ){
+        if(chart.data.datasets[1].data[i] >= 10)
+          chart.data.datasets[1].data[i] = 10.00;
+      }
 
-  chart.update();
+      chart.update();
+    }
+  });
 }
 
+/**
+ * @description 차트 기본 설정
+ */
 var config = {
-
+  /**
+   * @description x축 데이터
+   */
   xAxisDataSet: [],
+  /**
+   * @description y축 coins 데이터
+   */
   coinsDataSet: [],
+  /**
+   * @description y축 multiple 데이터
+   */
   multipleDataSet: [],
-
+  
+  // 차트 타입
   type: 'bar',
   data: {
     datasets: [{
@@ -46,42 +75,75 @@ var config = {
       data: this.coinsDataSet,
       type: 'bar',
       backgroundColor: "rgba(0,152,239,.75)",
+      yAxisID: 'y-axis-1'
     }, {
       label: 'multiple',
       data: this.multipleDataSet,
       type: 'line',
       backgroundColor: "transparent",
       borderColor: "#7C8792",
-      borderWidth: 1,
+      borderWidth: 2,
+      yAxisID: 'y-axis-2'
     }],
   labels: this.xAxisDataSet
   },
+
   options: {
+    // 마우스 hover했을 때 coins값과 multiple값을 동시에 보여주기 위해 작성
+    tooltips: {
+      mode: 'label'
+    },
     scales: {
-      yAxes: [{
-       ticks:{
-          min : 0,
-          stepSize : 50,
-          fontColor : "#5a697e",
-          fontSize : 14
+      yAxes: [
+      {
+        type: 'linear',
+        display: true,
+        position: 'left',
+        id: 'y-axis-1',
+        ticks: {
+          min: 0,
+          // 눈금 간격
+          stepSize: 30,
+          fontColor: "#5a697a",
+          fontSize: 14
         },
-      gridLines:{
-          color: "#28394a",
-          lineWidth:1,
-          // zeroLineColor :"#fff",
-          // zeroLineWidth : 1
+        labels: {
+          show: true
+        }
+      }, {
+        type: 'linear',
+        display: true,
+        position: 'right',
+        id: 'y-axis-2',
+        ticks: {
+          // 눈금 최대값 지정
+          max: 10,
+          // 눈금 뒤에 'x'를 붙이기 위해 작성
+          callback: function(label) {
+            return label+'x';
+          }
         },
-        stacked: true,
-      }],
+        labels: {
+          show: true
+        },
+        // multiple에 대한 grid line 그림
+        gridLines: {
+          drawOnChartArea: true,
+          color: '#28394a'
+        }
+      }
+    ],
 		xAxes: [{
       maxBarThickness: 10,
         ticks:{
           fontColor : "#5a697e",
-          fontSize : 14
+          fontSize : 14,
+          maxTicksLimit: 12,
+          maxRotation: 0
         },
+        // x축에 대한 grid line은 그리지 않음 따라서 가로줄 만 있음
         gridLines:{
-          color: "#28394a",
-          lineWidth:1
+          display: false
         },
         stacked: true,
       }]
@@ -93,11 +155,14 @@ var config = {
   }
 };
 
-
+// canvas DOM
 var ctx = document.getElementById('FEbCArea').getContext('2d');
+// Chart Object 생성
 var chartFrame = new Chart(ctx, config);
 
+// API call 하는 함수를 호출해 기본 값을 채워 넣음
 updateData(chartFrame);
+// 1초에 한번씩 반복해 정보가 업데이트 될 때 마다 차트를 갱신해줌
 setInterval(() => {
   updateData(chartFrame);
 }, 1000);
